0f649aed0d8b17cb79f10db1ee7e29679e821395
Jonathan Kitt <jonathan.kitt@inrae.fr>
2023-10-20 16:22:37 +0200

    update

--- 04_suivi_version/04_suivi_version.R
+++ 04_suivi_version/04_suivi_version.R
@@ -1,29 +1,74 @@
-# Configure git  ----------------------------------------------------------
-
-# git config --global user.name "Prenom Nom"
-# git config --global user.email prenom.nom@mail.com
-# git config --global core.editor "C:/
+# Sources -----------------------------------------------------------------
 
 
-# cd
-# mkdir tuto_git
-# cd tuto_git
 
+# Configure git -----------------------------------------------------------
+
+# git --version
+# git config --global user.name "Jonathan Kitt"
+# git config --global user.email jonathan.kitt@inrae.fr
+# git config --global --list
+
+# Getting help ------------------------------------------------------------
+
+# git help config
+# git config --help
+
+# Create a folder ---------------------------------------------------------
+
+# mkdir tuto_git_local
+# cd tuto_git_local
+# ls
+
+# Add files in dir --------------------------------------------------------
+
+# touch file1.txt
+# touch .project
+
+# Initialise a local git repo ---------------------------------------------
+
+# pwd
 # git init
 # ls
 # ls -la
 
-# echo hello > file1.txt
-# echo hello > file2.txt
+# to stop tracking our directory: rm -rf .git
 
-# ls
+# First commit ------------------------------------------------------------
+
 # git status
-# git add file1.txt file2.txt
-# git add *.txt
+# echo .project > .gitignore
+# ls -la
+# git status
+
+# git add -A
 # git add .
+# git add .gitignore
+# git add file1.txt
+# git add .gitignore file1.txt
 
+# to remove file from staging area: git reset
+# git reset file1.txt
+
+# git commit -m "initial commit"
+
+# git log
+
+# Apply changes  ----------------------------------------------------------
+
+# echo hello > file1.txt
+# cat file1.txt
+
+# show the changes made to the code: git diff
 # git status
 
-# git commit -m "ajout de deux fichiers texte"
-
+# git add -A
 # git status
+# git commit -m "add text in file1.txt"
+
+# Create a branch ---------------------------------------------------------
+
+# git branch file2
+# git branch
+# git checkout file2
+# git branch
